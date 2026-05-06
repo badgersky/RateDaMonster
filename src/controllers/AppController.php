86bbe10117a6a2d5c6f70455f2e0c4b0282e942a
szymon <szymon.pt4sznik@gmail.com>
2026-05-06 21:21:23 +0200

    added base app controller

---
+++ src/controllers/AppController.php
@@ -0,0 +1,36 @@
+<?php
+
+class AppController {
+    private $method;
+
+    public function __construct()
+    {
+        $this->method = $_SERVER['REQUEST_METHOD'];
+    }
+
+    protected function isGet(): bool
+    {
+        return $this->method === 'GET';
+    }
+
+    protected function isPost(): bool
+    {
+        return $this->method === 'POST';
+    }
+
+    protected function render(string $template = null, array $variables = [])
+    {
+        $templatePath = 'public/views/'.$template.'.php';
+        $output = 'File not found';
+
+        if (file_exists($templatePath)) {
+            extract($variables);
+
+            ob_start();
+            include $templatePath;
+            $output = ob_get_clean();
+        }
+
+        print $output;
+    }
+}
