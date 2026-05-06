b1ff7a16442e133f9ae5c758ee96f51970d08fa5
szymon <szymon.pt4sznik@gmail.com>
2026-05-06 19:05:36 +0200

    added routing.php with all ednpoints

---
+++ Routing.php
@@ -0,0 +1,71 @@
+<?php
+
+require_once 'src/controllers/SecurityController.php';
+require_once 'src/controllers/MonsterController.php';
+require_once 'src/controllers/AdminController.php';
+
+class Routing {
+    public static $routes = [
+        'login' => [
+            'controller' => 'SecurityController',
+            'action' => 'login'
+        ],
+        'register' => [
+            'controller' => 'SecurityController',
+            'action' => 'register'
+        ],
+        'logout' => [
+            'controller' => 'SecurityController',
+            'action' => 'logout'
+        ],
+        'monsters' => [
+            'controller' => 'MonsterController',
+            'action' => 'monsters'
+        ],
+        'monsters/(\d+)' => [
+            'controller' => 'MonsterController',
+            'action' => 'monster'
+        ],
+        'admin/users' => [
+            'controller' => 'AdminController',
+            'action' => 'users'
+        ],
+        'admin/monsters' => [
+            'controller' => 'AdminController',
+            'action' => 'adminMonsters'
+        ],
+        'admin/add-monster' => [
+            'controller' => 'AdminController',
+            'action' => 'addMonster'
+        ],
+        '' => [
+            'controller' => 'SecurityController',
+            'action' => 'login'
+        ]
+    ];
+
+    private static $instances = [];
+
+    public static function run(string $path) {
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        foreach (self::$routes as $pattern => $config) {
+            $regex = "#^" . $pattern . "$#";
+
+            if (preg_match($regex, $path, $matches)) {
+                $controllerName = $config['controller'];
+                $action = $config['action'];
+
+                if (!isset(self::$instances[$controllerName])) {
+                    self::$instances[$controllerName] = new $controllerName;
+                }
+                
+                $controllerObj = self::$instances[$controllerName];
+                $id = $matches[1] ?? null;
+
+                $controllerObj->$action($id, $method);
+                return;
+            }
+        }
+    }
+}
