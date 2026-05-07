338725181e4adf7f55b9f79c57790f5c17801a89
szymon <szymon.pt4sznik@gmail.com>
2026-05-07 21:04:23 +0200

    added partial html for displaying messages, added validation in security controller, tested user logging and registering

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -1,43 +1,75 @@
 <?php
 
 require_once 'AppController.php';
+require_once __DIR__.'/../repositories/UserRepository.php';
 
 class SecurityController extends AppController {
 
     public function login() {
         if (!$this->isPost()) {
-            return $this->render('login', ['title' => 'Login Page']);
+            return $this->render('login', ['title' => 'Login']);
         }
-        
-        die(var_dump($_POST));
 
         $username = $_POST['username'];
         $password = $_POST['password'];
 
-        // TODO: get user from database
-        // TODO: password_verify
-        // TODO: start session and redirect
+        if (empty($username) || empty($password)) {
+            return $this->render('login', ['messages' => ['Please fill all fields!']]);
+        }
+
+        $userRepository = new UserRepository();
+        $user = $userRepository->getUser($username);
+
+        if (!$user) {
+            return $this->render('login', ['messages' => ['User not found!']]);
+        }
+
+        if (!password_verify($password, $user['password'])) {
+            return $this->render('login', ['messages' => ['Wrong password!']]);
+        }
+
+        session_start();
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['username'] = $user['username'];
+
+        $url = "http://$_SERVER[HTTP_HOST]";
+        header("Location: {$url}/monsters");
     }
 
     public function register() {
         if (!$this->isPost()) {
-            return $this->render('register', ['title' => 'Register Account']);
+            return $this->render('register', ['title' => 'Register']);
         }
 
-        die(var_dump($_POST));
-
         $username = $_POST['username'];
-        $email = $_POST['email'];
         $password = $_POST['password'];
+        $confirmedPassword = $_POST['confirmedPassword'];
 
-        // TODO: validation
-        // TODO: password_hash
-        // TODO: saving to database
-        // TODO: redirect to login
+        if (empty($username) || empty($password) || empty($confirmedPassword)) {
+            return $this->render('register', ['messages' => ['All fields are required!']]);
+        }
+
+        if ($password !== $confirmedPassword) {
+            return $this->render('register', ['messages' => ['Passwords do not match!']]);
+        }
+
+        $userRepository = new UserRepository();
+        
+        try {
+            $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+            $userRepository->addUser($username, $hashedPassword);
+        } catch (PDOException $e) {
+            return $this->render('register', ['messages' => ['Username already taken!']]);
+        }
+
+        return $this->render('login', ['messages' => ['Registration successful!']]);
     }
 
     public function logout() {
-        // TODO: session_destroy
-        // TODO: redirect to login
+        session_start();
+        session_destroy();
+        
+        $url = "http://$_SERVER[HTTP_HOST]";
+        header("Location: {$url}/login");
     }
 }
