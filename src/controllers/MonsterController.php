894c985b4717fe1ea6b149c9bd7d18d05fdcb2be
szymon <szymon.pt4sznik@gmail.com>
2026-05-08 12:16:17 +0200

    udpdated monsters.html so it displays form for rating, functionality that only logged in users can rate monsters

--- src/controllers/MonsterController.php
+++ src/controllers/MonsterController.php
@@ -2,12 +2,21 @@
 
 require_once 'AppController.php';
 require_once __DIR__.'/../repositories/MonsterRepository.php';
+require_once __DIR__.'/../repositories/RatingRepository.php';
 
 class MonsterController extends AppController {
 
     public function monsters() {
+        session_start();
         $monsterRepository = new MonsterRepository();
+        $ratingRepository = new RatingRepository();
+        
         $monsters = $monsterRepository->getMonsters();
+
+        foreach ($monsters as &$monster) {
+            $avg = $ratingRepository->getAverageRating($monster['id']);
+            $monster['avg_rating'] = $avg ? round($avg, 1) : 'No ratings';
+        }
 
         $this->render('monsters', [
             'monsters' => $monsters,
@@ -16,21 +25,28 @@ class MonsterController extends AppController {
     }
 
     public function monster($id) {
-        $monsterRepository = new MonsterRepository();
-        $monster = $monsterRepository->getMonster($id);
-
-        if (!$monster) {
-            die("Monster not found!");
-        }
-
         if ($this->isPost()) {
-            $rating = $_POST['rating'] ?? 0;
-            die("Received rating: " . $rating . " for Monster: " . $monster['name']);
+            session_start();
+            
+            if (!isset($_SESSION['user_id'])) {
+                $url = "http://$_SERVER[HTTP_HOST]";
+                header("Location: {$url}/login");
+                exit();
+            }
+
+            $userId = $_SESSION['user_id'];
+            $rating = (int)($_POST['rating'] ?? 0);
+
+            if ($rating >= 1 && $rating <= 10) {
+                $ratingRepository = new RatingRepository();
+                $ratingRepository->addRating($userId, (int)$id, $rating);
+            }
+
+            header("Location: /monsters");
+            exit();
         }
 
-        $this->render('monster-details', [
-            'monster' => $monster,
-            'title' => 'Monster Details'
-        ]);
+        header("Location: /monsters");
+        exit();
     }
 }
