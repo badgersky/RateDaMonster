0d526c6fee7c2df3390d33ed9aa58358b1d7ac21
szymon <szymon.pt4sznik@gmail.com>
2026-05-08 11:40:42 +0200

    updated monsters.html so it actually displays information about monsters from database. Updated MonsterController so it fetches data from the database

--- src/controllers/MonsterController.php
+++ src/controllers/MonsterController.php
@@ -1,19 +1,36 @@
 <?php
 
 require_once 'AppController.php';
+require_once __DIR__.'/../repositories/MonsterRepository.php';
 
 class MonsterController extends AppController {
 
     public function monsters() {
-        $this->render('monsters', ['title' => 'Monster List']);
+        $monsterRepository = new MonsterRepository();
+        $monsters = $monsterRepository->getMonsters();
+
+        $this->render('monsters', [
+            'monsters' => $monsters,
+            'title' => 'Monster List'
+        ]);
     }
 
     public function monster($id) {
-        if ($this->isPost()) {
-            $rating = $_POST['rating'] ?? 0;
-            die("Received rating: " . $rating . " for Monster ID: " . $id);
+        $monsterRepository = new MonsterRepository();
+        $monster = $monsterRepository->getMonster($id);
+
+        if (!$monster) {
+            die("Monster not found!");
         }
 
-        $this->render('monster-details', ['title' => 'Monster Details', 'id' => $id]);
+        if ($this->isPost()) {
+            $rating = $_POST['rating'] ?? 0;
+            die("Received rating: " . $rating . " for Monster: " . $monster['name']);
+        }
+
+        $this->render('monster-details', [
+            'monster' => $monster,
+            'title' => 'Monster Details'
+        ]);
     }
 }
